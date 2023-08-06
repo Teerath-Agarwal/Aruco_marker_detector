1618c69a72864ae737f0bf382b048d1f6d594085
Teerath <teerath173@gmail.com>
2023-08-07 00:45:08 +0530

    minor bugs

--- Automation.py
+++ Automation.py
@@ -2,10 +2,6 @@ import cv2
 import numpy as np
 import cv2.aruco as aruco
 
-
-
-
-
 VideoCap = False
 capture = cv2.VideoCapture(0)
 
@@ -17,12 +13,11 @@ detector = cv2.aruco.ArucoDetector(dictionary, parameters)
 def findAruco(img,detector):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(gray)
-    for i in range(6):
+    for i in range(len(markerIds)):
         pt1 = tuple(markerCorners[i][0][0].astype(int))
         pt2 = tuple(markerCorners[i][0][2].astype(int))
         cv2.rectangle(img, pt1, pt2, (0,255,0), thickness=2)
-        text_position = (pt1[0], pt1[1])  
-        cv2.putText(img, str(markerIds[i]), text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(img, str(markerIds[i]), pt1, cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
     
 while True:
     
